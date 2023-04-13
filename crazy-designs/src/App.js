8201c12acf0d6e895e0c80e1cbb77d363d875e29
ezhilarasanm-toast <ezhilarasan1232000@gmail.com>
2023-04-13 18:50:11 +0530

    initial commit

---
+++ crazy-designs/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import MainPage from "./component/mainPage/MainPage.tsx";
+import MainPage1 from "./component/mainPage/MainPage1.tsx";
+
+function App() {
+  return (
+    <>
+      <MainPage></MainPage>
+    </>
+  );
+}
+
+export default App;
